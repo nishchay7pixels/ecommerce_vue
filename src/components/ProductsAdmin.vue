f544e16429124c0e70a9242f7910d2d6ff60c357
Nishchay Verma <nishchay.verma.7pixels@gmail.com>
2021-06-06 19:57:08 +0530

    feat: product page work almost done. Now support multiple images

--- src/components/ProductsAdmin.vue
+++ src/components/ProductsAdmin.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="products">
-    <h3>Add products</h3>
+    <h2>Products</h2>
+    <br />
     <div class="product-test">
+      <h5>Add/Edit</h5>
+      <br />
       <div class="form-group">
         <div class="container">
           <div class="row">
@@ -41,7 +44,9 @@
                   >{{ tag }}</span
                 >
               </label>
+
               <div class="mb-3">
+                <br />
                 <input
                   class="form-control"
                   @change="uploadImage"
@@ -55,16 +60,45 @@
               </div>
             </div>
           </div>
+          <div class="row">
+            <div
+              class="col"
+              v-for="(image, index) in product.data.image"
+              :key="index"
+            >
+              <div class="img-tile">
+                <span class="delete-img" @click="deleteImage(image, index)"
+                  >X</span
+                >
+                <img :src="image" width="80px" alt="images" />
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <br /><br />
+              <div class="form-group form-buttons">
+                <button
+                  @click="saveData()"
+                  v-if="!updateFlag"
+                  class="btn btn-primary"
+                >
+                  Save
+                </button>
+                <button
+                  @click="updateData()"
+                  v-if="updateFlag"
+                  class="btn btn-primary"
+                >
+                  Update
+                </button>
+                <button @click="clearForm()" class="btn btn-danger">
+                  Clear
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div class="form-group">
-        <button @click="saveData()" v-if="!updateFlag" class="btn btn-primary">
-          Save Product
-        </button>
-        <button @click="updateData()" v-if="updateFlag" class="btn btn-primary">
-          Update Product
-        </button>
-        <button @click="clearForm()" class="btn btn-danger">Clear</button>
       </div>
     </div>
     <br /><br />
@@ -113,7 +147,7 @@ export default {
           name: null,
           description: null,
           tags: [],
-          image: null,
+          image: [],
         },
       },
       products: [],
@@ -125,43 +159,58 @@ export default {
     this.watcher();
   },
   methods: {
+    deleteImage(img, index) {
+      console.log(index);
+      let image = fb.storage().refFromURL(img);
+      this.product.data.image.splice(index, 1);
+      image
+        .delete()
+        .then(() => {
+          console.log("Image Deleted");
+        })
+        .catch((error) => {
+          console.log("Error Occured" + error);
+        });
+    },
     uploadImage(e) {
       let file = e.target.files[0];
-      console.log(file);
-      var storageRef = fb.storage().ref("productsImage/" + file.name);
-      let uploadTask = storageRef.put(file);
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          // Observe state change events such as progress, pause, and resume
-          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-          var progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log("Upload is " + progress + "% done");
-          if(progress==100){
-            this.fileuploaded = true;
-            
+      if (file) {
+        console.log(file);
+        var storageRef = fb.storage().ref("productsImage/" + file.name);
+        let uploadTask = storageRef.put(file);
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            var progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            if (progress == 100) {
+              this.fileuploaded = true;
+            }
+            switch (snapshot.state) {
+              case fb.storage.TaskState.PAUSED: // or 'paused'
+                console.log("Upload is paused");
+                break;
+              case fb.storage.TaskState.RUNNING: // or 'running'
+                console.log("Upload is running");
+                break;
+            }
+          },
+          (error) => {
+            console.error(error);
+          },
+          () => {
+            uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+              let url = downloadURL;
+              console.log(url);
+              this.product.data.image.push("" + url);
+              console.log();
+            });
           }
-          switch (snapshot.state) {
-            case fb.storage.TaskState.PAUSED: // or 'paused'
-              console.log("Upload is paused");
-              break;
-            case fb.storage.TaskState.RUNNING: // or 'running'
-              console.log("Upload is running");
-              break;
-          }
-        },
-        (error) => {
-          console.error(error);
-        },
-        () => {
-          uploadTask.snapshot.ref.getDownloadURL().then( (downloadURL) => {
-            let url = downloadURL;
-            console.log(url);
-            this.product.data.image = ""+url;
-          });
-        }
-      );
+        );
+      }
     },
     clearForm() {
       this.resetData();
@@ -180,18 +229,27 @@ export default {
       });
     },
     saveData() {
-      this.product.data.price = "$" + this.product.data.price;
+      if (
+        this.product.data.name == null ||
+        this.product.data.price == null ||
+        this.product.data.name == ''||
+        this.product.data.price == ''
+      ) {
+        alert("Enter Required details");
+      } else {
+        this.product.data.price = "$" + this.product.data.price;
 
-      db.collection("Products")
-        .add(this.product.data)
-        .then((docRef) => {
-          console.log("Document Id:", docRef.id);
-          this.resetData();
-          this.getAll();
-        })
-        .catch((error) => {
-          console.error("Error adding document", error);
-        });
+        db.collection("Products")
+          .add(this.product.data)
+          .then((docRef) => {
+            console.log("Document Id:", docRef.id);
+            this.resetData();
+            this.getAll();
+          })
+          .catch((error) => {
+            console.error("Error adding document", error);
+          });
+      }
     },
     resetData() {
       Object.assign(this.$data, this.$options.data.apply(this)); //another way to reset data
@@ -213,18 +271,28 @@ export default {
       this.updateFlag = true;
     },
     updateData() {
-      this.storageRef.put();
-      console.log(this.product.id);
-      var item = db.collection("Products").doc(this.product.id);
-      return item
-        .update(this.product.data)
-        .then(() => {
-          console.log("Data updated");
-          this.updateFlag = false;
-        })
-        .catch((err) => {
-          console.error(err);
-        });
+      if (
+        this.product.data.name == null ||
+        this.product.data.price == null ||
+        this.product.data.name == ''||
+        this.product.data.price == ''
+      ) {
+        alert("Enter Required details");
+      } else {
+        console.log(this.product.id);
+        var item = db.collection("Products").doc(this.product.id);
+        return item
+          .update(this.product.data)
+          .then(() => {
+            console.log("Data updated");
+            this.updateFlag = false;
+            this.resetData();
+            this.getAll();
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      }
     },
     deleteProduct(id) {
       if (confirm("Are you sure?")) {
@@ -246,8 +314,19 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .products {
-  margin-top: 7rem;
+  margin-top: 2rem;
   background: #f2f2f2;
-  padding-bottom: 3rem;
+}
+
+.img-tile {
+  position: absolute;
+}
+.img-tile span.delete-img {
+  position: absolute;
+  top: -18px;
+  left: -2px;
+}
+.img-tile span.delete-img:hover {
+  cursor: pointer;
 }
 </style>
