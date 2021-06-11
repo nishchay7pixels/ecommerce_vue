3287185fcabbedb11fa04d1b74df71ad0455db59
Nishchay Verma <nishchay.verma.7pixels@gmail.com>
2021-06-12 02:06:25 +0530

    feat : cart items now also stored in browser local storage

--- src/store.js
+++ src/store.js
@@ -2,28 +2,36 @@ import Vuex from 'vuex'
 import Vue from 'vue'
 Vue.use(Vuex)
 
+let localCart = window.localStorage.getItem('cart');
+
 export default new Vuex.Store({
-    
+
     state: {
-        cart:[]
+        cart: localCart ? JSON.parse(localCart) : []
     },
-    mutations:{
-        addToCart(state, item){
+    mutations: {
+        addToCart(state, item) {
             let found = state.cart.find(product => product.productId == item.productId); //find product using id
-            if(found){ 
+            if (found) {
                 //if product is found increase quantity
                 found.productQuantity++;
-            }else{ 
+            } else {
                 //else add the item to cart
                 state.cart.push(item);
             }
+            this.commit('savaToLocal');
         },
-        deleteFromCart(state, pid){
+        deleteFromCart(state, pid) {
             let index = state.cart.findIndex(product => product.productId == pid);
-            state.cart.splice(index, 1)
-        }
+            state.cart.splice(index, 1);
+            this.commit('savaToLocal');
+        },
+        savaToLocal(state) {
+            window.localStorage.setItem('cart', JSON.stringify(state.cart));
+        },
+
     },
-    computed:{
+    computed: {
 
     }
 })
