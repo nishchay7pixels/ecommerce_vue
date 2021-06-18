a28c42c7871280330bc5b26b4b9f11c697e0b563
Nishchay Verma <nishchay.verma.7pixels@gmail.com>
2021-06-19 01:29:59 +0530

    feat : Implemented Product details page and setup route with product id

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,10 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    path: "*",
+    component: { render: (h) => h("div", ["404! Page Not Found!"]) },
+  },
+  {
     path: '/',
     redirect: '/home'
   },
@@ -31,6 +35,15 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "products" */ '../views/Products.vue')
+  },
+  {
+    path: "/productdetail/:id",
+    name: 'ProductDetails',
+    props: true,
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "products" */ '../views/ProductDetails.vue')
   },
   {
     path: '/checkout',
