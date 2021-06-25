c97810812633753b8e08a4adf3bb7307b71c5037
Nishchay Verma <nishchay.verma.7pixels@gmail.com>
2021-06-26 02:41:19 +0530

    feat : categoryTiles now fetched from database

--- src/sections/Category-Section.vue
+++ src/sections/Category-Section.vue
@@ -12,11 +12,11 @@ to that category. then your can just search for those product id in product db.
         <div
           class="row row-cols-2 row-cols-sm-2 row-cols-md-2 row-cols-lg-4 g-3"
         >
-          <div class="col" v-for="(offer, index) in offers" :key="index">
+          <div class="col" v-for="(tile, index) in categoryTiles" :key="index">
             <category-card
               class="card"
-              :image="offer.image"
-              :offerText="offer.offer_text"
+              :image="tile.data.categoryImage"
+              :offerText="tile.data.categoryTitle"
               :redirectTo="'#'"
             ></category-card>
           </div>
@@ -35,44 +35,19 @@ export default {
   props: {},
   data() {
     return {
-      brands: [],
-      offers: [
-        {
-          offer_text: "Bestseller",
-          image:
-            "https://firebasestorage.googleapis.com/v0/b/ecommerce-vue-29c36.appspot.com/o/offerAssets%2Fbestseller.PNG?alt=media&token=14689c9e-3178-4c79-b9a5-f7b7b5e3c5ad",
-        },
-        {
-          offer_text: "Heavy Discount!",
-          image:
-            "https://firebasestorage.googleapis.com/v0/b/ecommerce-vue-29c36.appspot.com/o/offerAssets%2Fdiscount.PNG?alt=media&token=fba384d6-6920-4aa8-a147-76350a551cb8",
-        },
-        {
-          offer_text: "My Polo",
-          image:
-            "https://firebasestorage.googleapis.com/v0/b/ecommerce-vue-29c36.appspot.com/o/offerAssets%2Fpolo.PNG?alt=media&token=8bf65e9a-ca03-4eef-a85d-972ef3362c4d",
-        },
-        {
-          offer_text: "Best of Pride",
-          image:
-             "https://firebasestorage.googleapis.com/v0/b/ecommerce-vue-29c36.appspot.com/o/offerAssets%2Fpride.PNG?alt=media&token=d2e5700a-359d-427b-a367-e0686f3c8899",
-        }
-      ],
-      quantity: "1",
+      categoryTiles: []
     };
   },
   created() {
-    this.getAllBrands();
+    this.getAllCategoryTiles();
   },
   methods: {
-    getAllBrands() {
-      db.collection("Brands")
-        .limit(8)
+    getAllCategoryTiles() {
+      db.collection("TiledCategory")
         .get()
         .then((querySnapshot) => {
           querySnapshot.forEach((element) => {
-            this.brands.push({ id: element.id, data: element.data() });
-            console.log(element.id);
+            this.categoryTiles.push({ id: element.id, data: element.data() });
           });
         });
     },
